df50d542e304cea98911cb21a1d74ac838c6b958
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-04-09 13:44:11 -0400

    test

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,13 +2,14 @@ var submitButton = document.getElementById("searchSubmitBtn");
 
 function getMovieData() {
   // Just using the github fetch url to make sure it works
-  var requestUrl = "https://api.github.com/orgs/nodejs/repos";
+  var requestUrl = 'http://www.omdbapi.com/?i=tt&apikey=cf7767a2';
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      
       console.log(data);
     });
 }

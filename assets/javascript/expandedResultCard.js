2ac6dbad77a337be2b9c5a31154e49f94bb9b2dd
Adam Brock <a.paulbrock@gmail.com>
2022-04-13 14:46:38 -0400

    made the remove/add to favorites button functional

--- assets/javascript/expandedResultCard.js
+++ assets/javascript/expandedResultCard.js
@@ -1,23 +1,23 @@
 const mainCardEl = document.querySelector("#expandedInfoCard");
-
 const urlParams = new URLSearchParams(window.location.search);
 const imdbID = urlParams.get("imdbID");
-
 const today = moment();
+let faveList = JSON.parse(localStorage.getItem("favorites")) ?? [];
+let movieFaveIndex = null;
+let currentMovieInfo;
 
 function getMovieData() {
-  // Just using the github fetch url to make sure it works
   var requestUrl =
     `http://www.omdbapi.com/?i=` + imdbID + `&plot=full&apikey=cf7767a2`;
 
   fetch(requestUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (movieData) {
-        console.log(movieData);
+        currentMovieInfo = movieData;
         createCard(movieData);
       });
     } else {
-      // need to inform user that there's a 404 error if they get to this else
+      mainCardEl.innerHTML = "<h1>Error loading resource.</h1>";
     }
   });
 }
@@ -37,15 +37,15 @@ function createCard(movieData) {
         movieData.Ratings[i].Value +
         `</p>`;
     }
-  }else{
-    ratingsHTML = "<p>Ratings: N/A</p>"
+  } else {
+    ratingsHTML = "<p>Ratings: N/A</p>";
   }
   newCard.innerHTML =
     `<h2>` +
     movieData.Title +
     `</h2><button class="rmvFavBtn" data-imdbid="` +
     movieData.imdbID +
-    `">Remove</button><p>Runtime: ` +
+    `">Add to Favorites</button><p>Runtime: ` +
     movieData.Runtime +
     `</p><img src="` +
     movieData.Poster +
@@ -86,5 +86,46 @@ function createCard(movieData) {
     newCard.classList.add("alreadyReleasedCard");
     mainCardEl.append(newCard);
   }
+
+  for (let i = 0; i < faveList.length; i++) {
+    const removeBtnEl = document.querySelector(".rmvFavBtn");
+    if (faveList[i].imdbID === imdbID) {
+      movieFaveIndex = i;
+      removeBtnEl.innerText = "Remove from Favorites";
+      break;
+    }
+  }
 }
 getMovieData();
+
+function rmvBtnHandler() {
+  const removeBtnEl = document.querySelector(".rmvFavBtn");
+  if (movieFaveIndex === null) {
+    faveList.push(currentMovieInfo);
+    localStorage.setItem("favorites", JSON.stringify(faveList));
+    movieFaveIndex = faveList.length - 1;
+    removeBtnEl.innerText = "Remove from Favorites";
+  } else if (movieFaveIndex !== null) {
+    faveList.splice(movieFaveIndex, 1);
+    localStorage.setItem("favorites", JSON.stringify(faveList));
+    removeBtnEl.innerText = "Add to Favorites";
+    movieFaveIndex = null;
+  }
+}
+
+mainCardEl.addEventListener("click", function (event) {
+  event.stopPropagation;
+  const target = event.target;
+  //   determines if the target has the rmvFavBtn class and runs the appropriate function if so
+  if (target.classList.contains("rmvFavBtn")) {
+    rmvBtnHandler(target);
+  }
+  //   need to add an if function into this listener that references the "create calendar event" classes
+});
+
+// this eventListener will reload the page when the user navigates back to it; this accounts for the user adding the movie as a favorite and then
+// tabbing back to this page. If it's not reloaded after adding favorites from the search page
+// the present variables on this page could reset the localstorage to an earlier state
+window.addEventListener("focus", function () {
+  document.location.reload(true);
+});

7756732fddcb2cc540a5bdebee4b1e2bda2f7cc9
Adam Brock <a.paulbrock@gmail.com>
2022-04-13 15:21:00 -0400

    Added comments to expandedResultsCard.js

--- assets/javascript/expandedResultCard.js
+++ assets/javascript/expandedResultCard.js
@@ -1,11 +1,14 @@
 const mainCardEl = document.querySelector("#expandedInfoCard");
+// pulls the search parameter, which will be used to make an API call to the omdb API
 const urlParams = new URLSearchParams(window.location.search);
 const imdbID = urlParams.get("imdbID");
 const today = moment();
+// used to interact with the locally stored favorites list
 let faveList = JSON.parse(localStorage.getItem("favorites")) ?? [];
 let movieFaveIndex = null;
 let currentMovieInfo;
 
+// performs an API call using the URL parameter established earlier
 function getMovieData() {
   var requestUrl =
     `http://www.omdbapi.com/?i=` + imdbID + `&plot=full&apikey=cf7767a2`;
@@ -22,12 +25,15 @@ function getMovieData() {
   });
 }
 
+// creates card using info returned by the API call
 function createCard(movieData) {
   const parsedReleaseDate = moment(movieData.Released, "DD MMM YYYY").format(
     "MM/DD/YYYY"
   );
   let ratingsHTML = "";
   const newCard = document.createElement("div");
+
+  // the ratings array is of variable length, so I had to build it using a for loop
   if (movieData.Ratings.length > 0) {
     for (let i = 0; i < movieData.Ratings.length; i++) {
       ratingsHTML +=
@@ -38,8 +44,10 @@ function createCard(movieData) {
         `</p>`;
     }
   } else {
+    // default if there are no ratings yet, which is common for movies that haven't been released
     ratingsHTML = "<p>Ratings: N/A</p>";
   }
+  // this creates a new card and fills it with the desired information
   newCard.innerHTML =
     `<h2>` +
     movieData.Title +
@@ -72,9 +80,9 @@ function createCard(movieData) {
     `</p>` +
     ratingsHTML +
     `</p>`;
-  newCard.classList.add("favCard", "TBDclass");
-  newCard.dataset.imdbid = movieData.imdbID;
+  newCard.classList.add("favCard");
 
+  // decides whether or not to add a button to make a calendar event
   if (today.isBefore(parsedReleaseDate)) {
     newCard.classList.add("upcomingCard");
     const calendarEvtBtn = document.createElement("button");
@@ -86,7 +94,7 @@ function createCard(movieData) {
     newCard.classList.add("alreadyReleasedCard");
     mainCardEl.append(newCard);
   }
-
+// determines the position of the currently displayed movie in the favorites, and if it's not present in the favorites it leave the button's innerText as "Add to favorites"
   for (let i = 0; i < faveList.length; i++) {
     const removeBtnEl = document.querySelector(".rmvFavBtn");
     if (faveList[i].imdbID === imdbID) {
@@ -98,8 +106,10 @@ function createCard(movieData) {
 }
 getMovieData();
 
+
 function rmvBtnHandler() {
   const removeBtnEl = document.querySelector(".rmvFavBtn");
+  // determines the current position of the selected movie in the faveList array, or leaves it as null if it's not in the array
   for (let i = 0; i < faveList.length; i++) {
     if (faveList[i].imdbID === imdbID) {
       movieFaveIndex = i;
@@ -107,6 +117,7 @@ function rmvBtnHandler() {
       break;
     }
   }
+  // decides whether the movie is in faveList and adds or removes it
   if (movieFaveIndex === null) {
     faveList.push(currentMovieInfo);
     localStorage.setItem("favorites", JSON.stringify(faveList));
@@ -122,9 +133,9 @@ function rmvBtnHandler() {
 mainCardEl.addEventListener("click", function (event) {
   event.stopPropagation;
   const target = event.target;
-  //   determines if the target has the rmvFavBtn class and runs the appropriate function if so
+  //   determines if the target has the rmvFavBtn class and runs the rmvBtnHandler function if so
   if (target.classList.contains("rmvFavBtn")) {
-    rmvBtnHandler(target);
+    rmvBtnHandler();
   }
   //   need to add an if function into this listener that references the "create calendar event" classes
 });

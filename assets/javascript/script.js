e696c17a9e2b2a43de7e58a3498a17786a086a85
Da Kim <daeunkim127@gmail.com>
2022-04-16 13:27:13 -0400

    index functioning. Filter by time frame added.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,51 @@
 var submitButton = document.getElementById("searchSubmitBtn");
-
+var resultContainer = document.getElementById("result-container");
+var genreSelected=document.getElementById("genreid");
+var timeFrameSelected=document.getElementById("timeframeid")
+var todayDate=moment().format('YYYY-MM-DD')
 var baseUrl='https://api.themoviedb.org'
 var apiKey='19a1fd696d217cbc89d9176a5b94e4e6'
 
-var getMovieData = function () {
-  var apiUrl = baseUrl+'/3/discover/movie/?api_key='+apiKey+'&region=US&release_date.gte=2022-04-14&release_date.lte=2022-05-15&with_release_type=3'
+// var newDate1 = moment().add(1, "day").format("ddd, MMM Do");
 
-
-
-
-  
+var getMovieData = function (e) {
+  e.preventDefault();
+  resultContainer.textContent=''
+  var genreID=genreSelected.value
+  var date=moment().add(timeFrameSelected.value, "day").format("YYYY-MM-DD");
+  console.log(date)
+  console.log(todayDate)
+  var apiUrl = baseUrl+'/3/discover/movie/?api_key='+apiKey+'&region=US&release_date.gte='+todayDate+'&release_date.lte='+date+'&with_release_type=3&with_genres='+genreID+'&sort_by=popularity.desc&sort_by=release_date.asc'
+  console.log(apiUrl)
   fetch(apiUrl).then(function(response){
     if (response.ok) {
       response.json().then(function(data){
         console.log(data)
-        displayPopularMovies(data.results)
+
+        for (var i=0; i<15; i++){
+        
+          var resultTMDBId=data.results[i].id
+          var posterPath=data.results[i].poster_path
+          var posterUrl='https://image.tmdb.org/t/p/original/'+posterPath
+          var resultEl=document.createElement('article');
+         
+          resultEl.innerHTML='<img src="'+posterUrl+'"/><a>'+data.results[i].original_title+'</a><p>Release Date:'+data.results[i].release_date+'</p>'
+          resultEl.classList='tile is-child notification is-warning'
+          
+          //add to favorite button
+          //poster
+          resultContainer.appendChild(resultEl)
+
+          //if there is no movie
+      
+        } 
+        
       })
     }
   })
-}
-// getMovieData()
 
+  
+}
 
 
 submitButton.addEventListener("click", getMovieData);
@@ -28,12 +53,9 @@ submitButton.addEventListener("click", getMovieData);
 // Da Eun code here
 var submitButton = document.getElementById("searchSubmitBtn");
 var popularMovieContainer = document.getElementById('popularMovieContainer')
-// var baseUrl='https://api.themoviedb.org'
-// var apiKey='19a1fd696d217cbc89d9176a5b94e4e6'
-
 
 var popularMovies = function () {
-  var apiUrl = baseUrl+'/3/movie/popular?api_key='+apiKey+'&language=en-US&page=1'
+  var apiUrl = baseUrl+'/3/movie/now_playing?api_key='+apiKey+'&language=en-US&page=1/3/movie/now_playing?api_key='
   
   fetch(apiUrl).then(function(response){
     if (response.ok) {
@@ -47,46 +69,29 @@ var popularMovies = function () {
 popularMovies()
 
 var displayPopularMovies = function(popular) {
-  
+  console.log(popular)
   for (var i=0; i<5; i++){
-    var popularTitle = popular[i].original_title;
-    var popularMovieId=popular[i].id
-    console.log()
-
-    var popularEl=document.createElement('a');
-   
-    var popularTitleEl = document.createElement('span');
-    popularTitleEl.textContent = popularTitle;
-    popularTitleEl.style.fontWeight='bold'
-
-    popularEl.appendChild(popularTitleEl);
-
-    var plotEl = document.createElement('span');
-    plotEl.style.display='block'
-    
-    plotEl.innerHTML=popular[i].overview;
-  
-
-    popularEl.appendChild(plotEl);
-
+        
+    var popularTMDBId=popular[i].id
+    var posterPath=popular[i].poster_path
+    var posterUrl='https://image.tmdb.org/t/p/original/'+posterPath
+    var popularEl=document.createElement('div');
+    popularEl.innerHTML='<a><img src="'+posterUrl+'"/></a>'
+    popularEl.classList=''
     popularMovieContainer.appendChild(popularEl)
-
   } 
 }
 
-function upcomingMoviesGenerator () {
-  var upcomingUrl=baseUrl+'/3/discover/movie?api_key='+apiKey+'&language=en-US&page=1&region=US&release_date_gte=2022-4-11&release_date.desc&with_genres=35'
+// function upcomingMoviesGenerator () {
+//   var upcomingUrl=baseUrl+'/3/discover/movie?api_key='+apiKey+'&language=en-US&page=1&region=US&release_date_gte=2022-4-11&release_date.desc&with_genres=35'
 
-  console.log(upcomingUrl)
-  fetch(upcomingUrl).then(function(response){
-    response.json().then(function(data){
-      console.log(data)
-    })
-  })
-}
+//   console.log(upcomingUrl)
+//   fetch(upcomingUrl).then(function(response){
+//     response.json().then(function(data){
+//       console.log(data)
+//     })
+//   })
+// }
 
-  upcomingMoviesGenerator()
+//   upcomingMoviesGenerator()
 
-  // var genreList = {"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}
-
-  // https://api.themoviedb.org/3/discover/movie?api_key=[MY_KEY]&language=en-US&page=1&primary_release_year=2019&with_genres=28,12,80
